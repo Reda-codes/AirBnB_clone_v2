fcc690cb878565e7ff0037b8e3d5dbce21cdb1e1
Reda <muhammadredabh@gmail.com>
2022-12-22 17:18:02 +0100

    new edit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,40 @@
 #!/usr/bin/env bash
 # Script that sets up your web servers for the deployment
 
+
 sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "<h1>Hello World<h1>" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/curren
-t/;\n\t}\n" > /etc/nginx/sites-available/default
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+    location / {
+                try_files $uri $uri/ =404;
+                add_header X-Served-By $hostname;
+    }
+    location /redirect_me{
+        return 301 https://www.youtube.com/watch?v=y6120QOlsfU;
+    }
+    location /hbnb_static{
+        alias /data/web_static/current/;
+    }
+    error_page 404 /custom_404.html;
+    location /custom_404.html {
+        root /var/www/html;
+        internal;
+    }
+}" > /etc/nginx/sites-available/default
 sudo service nginx restart
