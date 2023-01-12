511bc423e003c019ac75727026aa62888103ef7a
Reda <muhammadredabh@gmail.com>
2023-01-12 16:42:56 +0100

    2. C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Flask start module'''
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return 'HBNB!'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_route(text):
+    return 'C %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
