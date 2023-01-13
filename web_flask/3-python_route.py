f6e15725d6bb3bd7a5e02efe96ff4d35629bb20a
Reda <muhammadredabh@gmail.com>
2023-01-13 09:17:50 +0100

    fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -19,6 +19,7 @@ def hbnb_route():
 def text_route(text):
     return 'C %s' % text.replace('_', ' ')
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text='is cool'):
